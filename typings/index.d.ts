61d34d03fd5b44851bd6d537f9ad1165f47a1176
Erik Bigler <erikbigler@gmail.com>
2021-04-16 14:33:06 -0500

    Add methods for getting and setting command permissions

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -37,22 +37,55 @@ declare module "discord-slash-commands-client" {
     options?: ApplicationCommandOption[];
   }
 
+  /**
+   * Application command permissions allow you to enable or disable commands for specific users or roles within a guild.
+   * {@link https://discord.com/developers/docs/interactions/slash-commands#applicationcommandpermissions See discord docs}
+   */
+  interface ApplicationCommandPermissions {
+    /** Id of the role or user */
+    id: string;
+
+    /** The type of permission (1 = Role, 2 = User) */
+    type: 1 | 2;
+
+    /** `true` to allow, `false` to disallow */
+    permission: boolean;
+  }
+
+  /**
+   * Returned when fetching the permissions for a command in a guild.
+   * {@link https://discord.com/developers/docs/interactions/slash-commands#guildapplicationcommandpermissions See discord docs}
+   */
+  interface GuildApplicationCommandPermissions {
+    /** Id of the command */
+    id: string;
+
+    /** Id of the application the command belongs to */
+    application_id: string;
+
+    /** Id of the guild */
+    guild_id: string;
+
+    /** Array of ApplicationCommandPermissions */
+    permissions: ApplicationCommandPermissions[];
+  }
+
   export class Client {
     constructor(token: string, clientID: string);
     private token: string;
     private clientID: string;
-    public getCommands(
-      options?: getCommandsOptions
-    ): Promise<ApplicationCommand[] | ApplicationCommand>;
-    public createCommand(
-      options: ApplicationOptions,
-      guildID?: string
-    ): Promise<ApplicationCommand>;
-    public editCommand(
-      options: ApplicationOptions,
-      commandID: string,
-      guildID?: string
-    ): Promise<ApplicationCommand>;
+    public getCommands(options?: getCommandsOptions): Promise<ApplicationCommand[] | ApplicationCommand>;
+    public createCommand(options: ApplicationOptions, guildID?: string): Promise<ApplicationCommand>;
+    public editCommand(options: ApplicationOptions, commandID: string, guildID?: string): Promise<ApplicationCommand>;
     public deleteCommand(commandID: string, guildID?: string): Promise<boolean>;
+    public getCommandPermissions(
+      guildID: string,
+      commandID?: string
+    ): Promise<GuildApplicationCommandPermissions[] | GuildApplicationCommandPermissions>;
+    public editCommandPermissions(
+      permissions: ApplicationCommandPermissions[],
+      guildID: string,
+      commandID: string
+    ): Promise<GuildApplicationCommandPermissions>;
   }
 }
